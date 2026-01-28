11e47df7a24e13dd692b95a635c0608719484b05
Maks Zal <mzal990.b@gmail.com>
2026-01-28 22:26:06 +0000

    Updated trainingProgressionLogic.js
    Signed-off-by: Maks Zal <mzal990.b@gmail.com>

--- collegeGM2/client/src/data/trainingProgressionLogic.js
+++ collegeGM2/client/src/data/trainingProgressionLogic.js
@@ -1,7 +1,7 @@
-import { addXp } from "/data/seedGenerator";
+import { applyXp } from "/data/seedGenerator";
 
 function trainPlayer(player, attribute) {
   const updated = structuredClone(player);
-  addXp(updated, attribute, 25);
+  applyXp(updated, attribute, 25);
   db.players.put(updated);
 }
